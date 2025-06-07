32ae6c3c2a3b306b3f237606c800f03818fe5fcb
Cesar <cerh_dev@outlook.com>
2025-06-06 18:39:02 -0600

    create cards on pj3

--- projects/project3/script.js
+++ projects/project3/script.js
@@ -10,15 +10,58 @@ const fetchData = async function () {
     }
 
     const data = await response.json();
-
     getData(data);
   } catch (error) {
     console.error(error);
   }
 };
 
-const getData = function (titles) {
-  console.log(titles);
+// onWeek
+const renderData = (data, period, label) => {
+  const cards = document.querySelectorAll(".card");
+
+  data.forEach((item, index) => {
+    if (cards[index]) {
+      cards[index].innerHTML = `
+      <div class="current">
+        <div class="title">${item.title}</div>
+        <div class="current_period">${item.timeframes[period].current} hrs</div>
+      </div>
+
+      <div class="previous">
+        <img
+          src="./assets/icon-ellipsis.svg"
+          alt="ellipsis icon"
+          class="ellipsis"
+        />
+        <div class="previous_period">${label} - ${item.timeframes[period].previous} hrs</div>
+      </div>      
+      `;
+    }
+  });
+};
+
+const onDay = (data) => renderData(data, "daily", "Yesterday");
+const onWeek = (data) => renderData(data, "weekly", "Last Week");
+const onMonth = (data) => renderData(data, "monthly", "Last Month");
+
+const getData = (data) => {
+  const daily = document.getElementById("daily");
+  const weekly = document.getElementById("weekly");
+  const monthly = document.getElementById("monthly");
+
+  const addClickListener = (element, callback) => {
+    if (element) {
+      element.addEventListener("click", callback);
+    }
+  };
+
+  addClickListener(daily, () => onDay(data));
+  addClickListener(weekly, () => onWeek(data));
+  addClickListener(monthly, () => onMonth(data));
+
+  // Default view: Weekly
+  onWeek(data);
 };
 
 fetchData();
