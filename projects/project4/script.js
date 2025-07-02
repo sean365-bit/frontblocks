3562e4c8a35c637dccf74fff06f28353f352a6cb
Cesar <cerh_dev@outlook.com>
2025-07-01 18:04:17 -0600

    add semantics

--- projects/project4/script.js
+++ projects/project4/script.js
@@ -2,20 +2,13 @@
 console.log("This is project 4, this was developed by sean365-bit");
 
 const toggle = document.getElementById("modeToggle");
+const modeText = document.querySelector(".mode_text");
 
 function itemsToChange() {
   const element = document.body;
+
   element.classList.toggle("dark_mode");
+  modeText.textContent = toggle.checked ? "Dark Mode" : "Light Mode";
 }
 
 toggle.addEventListener("change", itemsToChange);
-
-// const buttonLight = document.querySelector(".slider");
-/*
-If you'd like the text to change from “Dark Mode” to “Light Mode” based on the current theme, update it in JS:
-
-const modeText = document.querySelector(".mode_text");
-function updateModeText(isDark) {
-  modeText.textContent = isDark ? "Light Mode" : "Dark Mode";
-}
-*/
