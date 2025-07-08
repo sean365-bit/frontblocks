ddd35d3774cd0368b001122cb362fb16120c0fd3
Cesar <cerh_dev@outlook.com>
2025-07-08 12:55:40 -0600

    add images to pj5

--- projects/project5/script.js
+++ projects/project5/script.js
@@ -1,2 +1,30 @@
 "use strict";
 console.log("This is project 5, this was developed by sean365-bit");
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
