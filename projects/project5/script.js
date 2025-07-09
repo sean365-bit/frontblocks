4442592e7e178cdad9116ae31863104f93ea3be0
Cesar <cerh_dev@outlook.com>
2025-07-09 15:56:19 -0600

    Add navbar

--- projects/project5/script.js
+++ projects/project5/script.js
@@ -1,30 +1,60 @@
 "use strict";
 console.log("This is project 5, this was developed by sean365-bit");
 
-let slideIndex = 1;
-showSlides(slideIndex);
+function initSlideshow({ slidesSelector, prevBtnId, nextBtnId }) {
+  const slides = document.getElementsByClassName(slidesSelector);
+  const previous = document.getElementById(prevBtnId);
+  const next = document.getElementById(nextBtnId);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+  let slideIndex = 1;
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+  function showSlides(n) {
+    if (n > slides.length) slideIndex = 1;
+    if (n < 1) slideIndex = slides.length;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    // Show the current slide
+    slides[slideIndex - 1].style.display = "block";
   }
 
-  slides[slideIndex - 1].style.display = "block";
+  function changeSlide(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+  }
+
+  // Initial render
+  showSlides(slideIndex);
+
+  // Event listeners (with correct callbacks)
+  previous.addEventListener("click", () => changeSlide(-1));
+  next.addEventListener("click", () => changeSlide(1));
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  initSlideshow({
+    slidesSelector: "mySlides",
+    prevBtnId: "previous",
+    nextBtnId: "next",
+  });
+});
+
+/* mine */
+// navMenu
+
+const menu = document.getElementById("menu");
+
+function myFunction() {
+  const navMenu = document.getElementById("myTopnav");
+
+  if (navMenu.className === "topnav") {
+    navMenu.className += " responsive";
+  } else {
+    navMenu.className = "topnav";
+  }
+}
+
+menu.addEventListener("click", myFunction);
